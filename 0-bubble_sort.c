9b07b965d5dbde26132908a07be8d8fa6630bac9
OHind <ouchani.hind@gmail.com>
2023-08-27 08:30:09 -0700

    Sorting algorithms : buble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swipe_elements - swipe two elements
+ * @a: The first variable
+ * @b: The second variable
+ * Return: void
+ */
+void swipe_elements(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - sorting an array following the algorith buble sort
+ * @array: The array to sort
+ * @size: The size of the array
+ * Returns: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, index, tmp;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
